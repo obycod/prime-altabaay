6e031dca79000118f137d4815cc7a3a54a11bbe5
obycod <obycode23@gmail.com>
2026-05-19 19:54:33 +0300

    1. إغلاق الأبواب الخلفية (API Endpoints Protection) - 🔴 (أهم خطوة حالياً)

--- get_orders.php
+++ get_orders.php
@@ -1,5 +1,13 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    http_response_code(403);
+    echo json_encode(['success' => false, 'error' => 'غير مصرح لك بالوصول! يرجى تسجيل الدخول.']);
+    exit;
+}
+
 require 'db.php';
 
 try {
